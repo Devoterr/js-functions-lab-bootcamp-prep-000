c4256466d5eada704ea475186b5cc5629ae9b352
Carlos Martinez <cmartinez88@gmail.com>
2019-10-10 01:11:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(Meesa) {
+  return "Happy holidays, ${name}!";
+}
+
+function happyHolidayTo([Christmas, Meesa]) {
+  
+}
